88793091bd04a7f9638900dad059d848498b418e
FregEscape3 <jbie1@hwemail.com>
2026-04-29 19:11:44 -0700

    Create sorts.java

---
+++ Unit 6 Exercises/sorts.java
@@ -0,0 +1,88 @@
+import java.util.Arrays;
+
+public class sorts {
+    public static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int current = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > current) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = current;
+        }
+    }
+
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+
+    public static void mergeSort(int[] arr) {
+        if (arr.length <= 1) {
+            return;
+        }
+
+        int mid = arr.length / 2;
+        int[] left = Arrays.copyOfRange(arr, 0, mid);
+        int[] right = Arrays.copyOfRange(arr, mid, arr.length);
+
+        mergeSort(left);
+        mergeSort(right);
+        merge(arr, left, right);
+    }
+
+    private static void merge(int[] arr, int[] left, int[] right) {
+        int leftIndex = 0;
+        int rightIndex = 0;
+        int mergedIndex = 0;
+
+        while (leftIndex < left.length && rightIndex < right.length) {
+            if (left[leftIndex] <= right[rightIndex]) {
+                arr[mergedIndex] = left[leftIndex];
+                leftIndex++;
+            } else {
+                arr[mergedIndex] = right[rightIndex];
+                rightIndex++;
+            }
+            mergedIndex++;
+        }
+
+        while (leftIndex < left.length) {
+            arr[mergedIndex] = left[leftIndex];
+            leftIndex++;
+            mergedIndex++;
+        }
+
+        while (rightIndex < right.length) {
+            arr[mergedIndex] = right[rightIndex];
+            rightIndex++;
+            mergedIndex++;
+        }
+    }
+}
